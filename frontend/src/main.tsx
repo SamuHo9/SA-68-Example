9794431e8d6351fae61df4cdc5b3699190f5c809
SamuHo9 <samsion0991713667@gmail.com>
2025-10-29 13:24:49 +0700

    gg

---
+++ frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+//backend
+//go build -o main.exe main.go
+//./main.exe
+
+//frontend
+//npm run dev
